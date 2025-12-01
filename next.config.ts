56903d165bd70d42db8d701142ba3ed1d89fc4ba
cmc1supertech601 <cmc1supertech601@gmail.com>
2025-11-30 20:12:03 -0600

    Fix image paths by updating basePath to /repo2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/repo2',
+  assetPrefix: '/repo2/',
+  images: {
+    unoptimized: true,
+  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
