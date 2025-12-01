5ab741720a701122febb2ca14ac3601bd09ec6a1
cmc1supertech601 <cmc1supertech601@gmail.com>
2025-11-30 19:57:28 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/dad-construction',
-  assetPrefix: '/dad-construction/',
-  images: {
-    unoptimized: true,
-  },
-  reactCompiler: true,
+  /* config options here */
 };
 
 export default nextConfig;
